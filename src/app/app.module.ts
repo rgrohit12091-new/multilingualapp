7db49123f6fd801b6fc0f392c5852fe062acbe6e
Rohit Goel <Rohit.Goel@quisitive.com>
2022-09-02 16:59:42 +0530

    Integration with Kendo UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ButtonComponent } from './button/button.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { ButtonsModule } from "@progress/kendo-angular-buttons";
 
 @NgModule({
   declarations: [
@@ -15,6 +18,8 @@ import { ButtonComponent } from './button/button.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, 
+    ButtonsModule,
     HttpClientModule,
     AppRoutingModule,
     TranslateModule.forRoot({
